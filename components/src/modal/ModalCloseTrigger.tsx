81be0554043c6ba6bdd0434cee394fd15b464f67
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-06-17 17:30:28 +0900

    feat: openTrigger, closeTrigger asChild 기능 구현

--- components/src/modal/ModalCloseTrigger.tsx
+++ components/src/modal/ModalCloseTrigger.tsx
@@ -1,31 +1,48 @@
-import { cloneElement, PropsWithChildren, ReactElement } from 'react';
+import {
+  cloneElement,
+  isValidElement,
+  PropsWithChildren,
+  ReactElement,
+} from 'react';
 import { useModalContext } from './ModalProvider';
-import { isTriggerButtonElement } from './utils/modalTriggerUtils';
 
-function ModalCloseTrigger({ children }: PropsWithChildren) {
+interface ModalCloseTriggerProps {
+  asChild?: boolean;
+}
+
+function ModalCloseTrigger({
+  asChild,
+  children,
+}: PropsWithChildren<ModalCloseTriggerProps>) {
   const { onClose } = useModalContext();
 
-  if (isTriggerButtonElement(children)) {
-    const element = children as ReactElement<{
-      onClick?: (e: React.MouseEvent) => void;
-    }>;
-    const originalOnClick = element.props.onClick;
-
-    return cloneElement(element, {
-      onClick: (event: React.MouseEvent) => {
-        if (typeof originalOnClick === 'function') {
-          originalOnClick(event);
-        }
-        onClose();
-      },
-    });
+  if (!asChild) {
+    return (
+      <button type="button" onClick={onClose} aria-label="모달 열기 버튼">
+        {children}
+      </button>
+    );
   }
 
-  return (
-    <button type="button" onClick={onClose}>
-      {children}
-    </button>
-  );
+  if (!isValidElement(children)) {
+    throw new Error(
+      '[Modal.CloseTrigger] asChild 사용 시 자식은 하나의 ReactElement여야 합니다.'
+    );
+  }
+
+  const element = children as ReactElement<{
+    onClick?: (e: React.MouseEvent) => void;
+  }>;
+  const originalOnClick = element.props.onClick;
+
+  return cloneElement(element, {
+    onClick: (event: React.MouseEvent) => {
+      if (typeof originalOnClick === 'function') {
+        originalOnClick(event);
+      }
+      onClose();
+    },
+  });
 }
 
 export default ModalCloseTrigger;
